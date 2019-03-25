a07cdb24bfa453088c0948f7c6ba9bacd30bb902
Kavana Bhat <Kavana.bhat@in.ibm.com>
2019-03-25 11:01:01 -0500

    AIX Changes

---
+++ aix_setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set +x
+cd third_party
+yes | rm -r googletest >/dev/null 2>&1
+yes | rm -r protobuf >/dev/null 2>&1
+git clone https://github.com/protocolbuffers/protobuf.git -b v3.6.0
+git clone https://github.com/kavanabhat/googletest.git -b master
+export CXXFLAGS="-mvsx -maix64"
+export CFLAGS="-mvsx -maix64"
+export CXX='g++ -L/opt/freeware/lib/pthread/ppc64 -lstdc++ -pthread'
+export LDFLAGS='-latomic -lpthread -Wl,-bbigtoc'
+export CC="gcc -L/opt/freeware/lib/pthread/ppc64 -pthread -maix64 -mvsx"
+exit 0
